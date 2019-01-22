1ef1246f88550a7880773ca15191c0d90e43d18a
root <root@localhost.localdomain>
2019-01-22 16:34:45 +0530

    function and modules

---
+++ func.py
@@ -0,0 +1,22 @@
+
+
+def motd():
+	print "this is day one"
+	print "this is day one"
+ 	print "this is day one"
+        print "this is day one"
+
+def sum(x,y):
+	return x+y
+
+def clear():
+	print "\n"*50
+
+motd()
+
+def sum1(*y):
+	data = 0
+	for i in y:
+		data=data+i
+		print data
+
