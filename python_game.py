78435d5244e06325e928a1d46f5bb5a9148553c7
root <root@station157.example.com>
2019-02-27 18:56:04 +0530

    Jumping&Boundaries

--- python_game.py
+++ python_game.py
@@ -6,11 +6,13 @@ win = pygame.display.set_mode((500,500))
 pygame.display.set_caption("First Game")
 
 x=50
-y=50
+y=425
 width=40
 height=60
 vel=5
 
+isJump = False
+jumpCount = 10
 run = True
 
 while run:
@@ -21,17 +23,32 @@ while run:
 			run = False
 	keys = pygame.key.get_pressed()
 
-	if keys[pygame.K_LEFT]:
+	if keys[pygame.K_LEFT] and x > vel:
 		x -= vel
 	
-	if keys[pygame.K_RIGHT]:
+	if keys[pygame.K_RIGHT] and x < 500 - width - vel:
 		x += vel
 	
-	if keys[pygame.K_UP]:
-		y -= vel
+        if not(isJump):
+
+		if keys[pygame.K_UP] and y > vel:
+			y -= vel
 	
-	if keys[pygame.K_DOWN]:
-		y += vel
+		if keys[pygame.K_DOWN] and y < 500 - height - vel:
+			y += vel
+        
+        	if keys[pygame.K_SPACE]:
+			isJump = True
+        else:
+		if jumpCount >= -10 :
+			neg = 1
+			if jumpCount < 0:
+				neg = -1
+            		y -= (jumpCount ** 2) * 0.5 * neg
+			jumpCount -= 1
+		else:
+			isJump = False
+			jumpCount = 10
         win.fill((0,0,0))
         pygame.draw.rect(win,(0,255,0),(x,y,width,height))
         pygame.display.update()
@@ -42,3 +59,4 @@ while run:
 
 
 pygame.quit()
+
